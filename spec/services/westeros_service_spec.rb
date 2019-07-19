91fb438e050e4f130bf897e21848ed6953ac185e
billy urrutia <billyurrutia@gmail.com>
2019-07-19 11:41:43 -0600

    changed service test

--- spec/services/westeros_service_spec.rb
+++ spec/services/westeros_service_spec.rb
@@ -2,13 +2,11 @@ require 'rails_helper'
 
 describe "Westeros service" do 
   it "should return a giant hash of members" do 
-    resp = WesterosService.new("greyjoy").parse_house_response
+    resp = WesterosService.new("5").parse_house_response
 
-    expect(resp.first).to have_key(:attributes)
-    expect(resp.first[:attributes]).to have_key(:members)
-    expect(resp.first[:attributes][:members].first).to have_key(:name)
-    expect(resp.first[:attributes][:members].first).to have_key(:id)
-    expect(resp.first[:attributes][:members].first[:name]).to eq("Balon Greyjoy")
-    expect(resp.first[:attributes][:members].first[:id]).to eq(38)
+    expect(resp.first).to have_key(:name)
+    expect(resp.first).to have_key(:id)
+    expect(resp.first[:name]).to eq("Balon Greyjoy")
+    expect(resp.first[:id]).to eq(38)
   end
 end
