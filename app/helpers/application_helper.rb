ac268d743c6f0266af7c9f6beb3fa574a128a4ba
billy urrutia <billyurrutia@gmail.com>
2019-07-19 11:41:03 -0600

    added ids to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
   def houses
     [
-      ['Stark', 'stark'],
-      ['Lannister', 'lannister'],
-      ['Targaryen', 'targaryen'],
-      ['Tyrell', 'tyrell'],
-      ['Greyjoy', 'greyjoy']
+      ['Stark', 'stark', 1],
+      ['Lannister', 'lannister', 2],
+      ['Targaryen', 'targaryen', 3],
+      ['Tyrell', 'tyrell', 4],
+      ['Greyjoy', 'greyjoy', 5]
     ]
   end
 end
